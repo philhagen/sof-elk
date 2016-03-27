684edde89b15beaed15adacd057447bdda6f1b19
Phil Hagen <phil@lewestech.com>
2016-03-27 13:30:44 -0400

    fix dashboard load script so it can be called via absolute path

--- dashboards/load_all_dashboards.sh
+++ dashboards/load_all_dashboards.sh
@@ -10,14 +10,15 @@ es_host=localhost
 es_port=9200
 kibana_index=.kibana
 dashboard_list="httpd introductory netflow syslog"
+dashboard_dir="/usr/local/for572logstash/dashboards/"
 
 for dashboard in ${dashboard_list}; do
-    for type in ${dashboard}/*; do
+    for type in ${dashboard_dir}${dashboard}/*; do
         type=$( basename $type )
-        for object in ${dashboard}/${type}/*; do
+        for object in ${dashboard_dir}${dashboard}/${type}/*; do
             object=$( basename ${object} )
             curl -s -XDELETE http://${es_host}:${es_port}/${kibana_index}/${type}/${object} > /dev/null
-            curl -s -XPUT http://${es_host}:${es_port}/${kibana_index}/${type}/${object} -T ${dashboard}/${type}/${object} > /dev/null
+            curl -s -XPUT http://${es_host}:${es_port}/${kibana_index}/${type}/${object} -T ${dashboard_dir}${dashboard}/${type}/${object} > /dev/null
         done
     done
 done
