ae64f668cc2f6e12472a202f220a87f5a8c0f8e9
Phil Hagen <phil@lewestech.com>
2016-03-26 20:04:46 -0400

    convert all dashboards, searches, and visualizations to to be more feasible to load via files from git.  includes first attempt at script to clear existing objects and load from files.

---
+++ dashboards/load_all_dashboards.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+es_host=localhost
+es_port=9200
+kibana_index=.kibana
+dashboard_list="httpd introductory netflow syslog"
+
+for dashboard in ${dashboard_list}; do
+    for type in ${dashboard}/*; do
+        type=$( basename $type )
+        for object in ${dashboard}/${type}/*; do
+            object=$( basename ${object} )
+            curl -s -XDELETE http://${es_host}:${es_port}/${kibana_index}/${type}/${object} > /dev/null
+            curl -s -XPUT http://${es_host}:${es_port}/${kibana_index}/${type}/${object} -T ${dashboard}/${type}/${object} > /dev/null
+        done
+    done
+done
