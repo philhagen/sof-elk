d8d23cbf0b6879cf94656cf636205c151341c7ab
root <mark.mccurdy@mail.com>
2016-07-27 02:06:13 +0000

    Decided on another rename of timeline template

--- supporting-scripts/load_all_dashboards.sh
+++ supporting-scripts/load_all_dashboards.sh
@@ -8,12 +8,12 @@ es_host=localhost
 es_port=9200
 kibana_index=.kibana
 
-index_patterns="httpdlog netflow syslog timeline-plaso"
+index_patterns="httpdlog netflow syslog timelineplaso"
 
 kibana_version=$( jq -r '.version' < /opt/kibana/package.json )
 kibana_build=$(jq -r '.build.number' < /opt/kibana/package.json )
 
-dashboard_list="httpd introductory netflow syslog timeline-plaso "
+dashboard_list="httpd introductory netflow syslog timelineplaso "
 dashboard_dir="/usr/local/sof-elk/dashboards/"
 
 # enter a holding pattern until the elasticsearch server is available, but don't wait too long
