adcaa3d703138176fbb6a216975f7dec7f07fd83
root <mark.mccurdy@mail.com>
2016-08-13 04:11:09 +0000

    Updated timeline template. es_nuke supports deleting indices with specific suffixes

--- supporting-scripts/es_nuke.sh
+++ supporting-scripts/es_nuke.sh
@@ -16,10 +16,17 @@ if [ ${ARGC} -ne 1 ]; then
 fi
 
 BASEINDEX=$1
-DOCCOUNT=$( curl -s http://localhost:9200/${BASEINDEX}-*/_count | jq '.count' )
+# If you provided the index including the value after the dash, don't append a -*
+if [ "`echo $BASEINDEX | grep -v '\-'`" ]; then
+  BASEINDEX="$BASEINDEX-*"
+fi
+DOCCOUNT=$(curl -s http://localhost:9200/${BASEINDEX}/_count | jq -e '.count')
+if [ $? == 1 ]; then
+  DOCCOUNT=0
+fi
 
 if [ ${DOCCOUNT} -eq 0 ]; then
-    echo -e "There are no documents in the '\e[1m${BASEINDEX}-*\e[0m' indices."
+    echo -e "There are no documents in the '\e[1m${BASEINDEX}\e[0m' indices."
     echo "Exiting."
     exit
 fi
@@ -28,18 +35,18 @@ echo "WARNING!!  THIS COMMAND CAN DESTROY DATA!  READ CAREFULY!"
 echo "---------------------------------------------------------"
 echo "This script will permanently delete data from the elasticsearch server."
 echo
-echo -e "There are currently \e[1m${DOCCOUNT}\e[0m documents in the '\e[1m${BASEINDEX}-*\e[0m' indices."
+echo -e "There are currently \e[1m${DOCCOUNT}\e[0m documents in the '\e[1m${BASEINDEX}\e[0m' indices."
 
 echo
-echo -e "To delete the '\e[1m${BASEINDEX}-*\e[0m' indices from the server, type \"\e[1mYES\e[0m\" below and press return."
+echo -e "To delete the '\e[1m${BASEINDEX}\e[0m' indices from the server, type \"\e[1mYES\e[0m\" below and press return."
 read RESPONSE
 
 if [ ${RESPONSE} != "YES" ]; then
     echo "Not deleting anything without explicit confirmation."
     exit
 else
-    echo -n "Deleting all data from the '${BASEINDEX}-* indices: "
-    curl -s -XDELETE "http://localhost:9200/${BASEINDEX}-*" > /dev/null && success || failure
+    echo -n "Deleting all data from the '${BASEINDEX} indices: "
+    curl -s -XDELETE "http://localhost:9200/${BASEINDEX}" > /dev/null && success || failure
     echo
 
     echo
