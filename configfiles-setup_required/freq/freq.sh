2166713dc3bd3c218d09e9a5356d49401e1f092f
SMAPPER <justin@hasecuritysolutions.com>
2016-11-09 11:13:00 -0600

    Revert "First merge attempt"
    This reverts commit 7f93cdb62fcfc12de0902ba7aad5fcded16f7908.

--- configfiles-setup_required/freq/freq.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-python /opt/freq/freq_server.py 10001 /opt/freq/dns.freq &
-python /opt/freq/freq_server.py 10002 /opt/freq/dns.freq &
-python /opt/freq/freq_server.py 10003 /opt/freq/english_lowercase.freq &
-python /opt/freq/freq_server.py 10004 /opt/freq/dns.freq &
