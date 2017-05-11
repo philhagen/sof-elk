9db739ba7aac4c8e7e696614b133af138b26c861
Phil Hagen <phil@lewestech.com>
2017-05-11 11:00:02 +0300

    add proper exit directives for each case

--- supporting-scripts/git-check-pull-needed.sh
+++ supporting-scripts/git-check-pull-needed.sh
@@ -7,17 +7,21 @@ BASE=$(git merge-base @{0} "$UPSTREAM")
 
 if [ $LOCAL = $REMOTE ]; then
     # up to date, nothing to do
+    exit 0
 
 elif [ $LOCAL = $BASE ]; then
     echo "Updates to the SOF-ELK configuration files are available!"
     echo "These are not required, but if desired, run the following to retrieve:"
     echo
     echo "sudo sof-elk_update.sh"
+    exit 0
 
 elif [ $REMOTE = $BASE ]; then
     # this should never happen - local copies won't push to origin
+    exit 1
 
 else
     # there should be no other case - this means some weird error occurred in gathering the SHA hashes
+    exit 1
 
 fi
