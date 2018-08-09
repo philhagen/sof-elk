06c6f3d1103f6dcfe19d55e681ddbdf99b6ee94e
Phil Hagen <phil@lewestech.com>
2018-08-09 14:16:33 -0400

    better dashboard export handling

--- supporting-scripts/admin/dump_dashboards.sh
+++ supporting-scripts/admin/dump_dashboards.sh
@@ -6,5 +6,24 @@
 
 # get list of all dashboard IDs to use for export and filenames
 for DASHID in $( curl -s "http://localhost:9200/.kibana/_search?q=type:dashboard&size=10000" | jq -r '.hits.hits[]._id[10:]' ); do
-    curl -s -XGET http://localhost:5601/api/kibana/dashboards/export?dashboard=${DASHID} | jq '.|del(.objects[].version)|del(.objects[].attributes.version)' > ${DASHID}.json
+
+    # get the dashboard content, filter to remove unnecessary fields
+    curl -s -XGET http://localhost:5601/api/kibana/dashboards/export?dashboard=${DASHID} > ${DASHID}_raw.json 2> /dev/null
+    cat ${DASHID}_raw.json | jq '.|del(.objects[].version)|del(.objects[].attributes.version)' > ${DASHID}.json 2> /dev/null
+    RES=$?
+    if [ $RES -eq 0 ]; then
+        rm -f ${DASHID}_raw.json
+    else
+        rm -f ${DASHID}.json
+        echo "WARNING!! ${DASHID} paring-down failed.  This must be manually performed - the source data likely is missing some closure brackets."
+        continue
+    fi
+
+    # remove the index-patterns on the intro dashboard
+    DASHTITLE=$( cat ${DASHID}.json | jq -r ".objects[] | select(.id == \"${DASHID}\") | .attributes.title" )
+    if [ "${DASHTITLE}" == "SOF-ELK® VM Introduction Dashboard" ]; then
+        cat ${DASHID}.json | jq ". | del(.objects[] | select(.type==\"index-pattern\"))" > ${DASHID}_temp.json
+        rm ${DASHID}.json
+        mv ${DASHID}_temp.json ${DASHID}.json
+    fi
 done
