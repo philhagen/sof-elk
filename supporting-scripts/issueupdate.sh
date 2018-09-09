b3b66939121f222330508de88dde0a341db2dec2
Phil Hagen <phil@lewestech.com>
2018-09-09 12:07:04 -0500

    expand post-issue update logging

--- supporting-scripts/issueupdate.sh
+++ supporting-scripts/issueupdate.sh
@@ -5,13 +5,14 @@
 # This script runs as a post-dhcp lease acquisition hook to change the /etc/issue file with a new IP address, then restarting any agetty processes so the new IP is shown on the prelogin authentiction screen
 
 issueupdate_config() {
-    logger "SOF-ELK post-dhcp lease: issueupdate_config()"
     IP=$( ip address show $( ip route|grep default | awk '{print $5}' ) | awk '/inet / {print $2}' | cut -d '/' -f 1 )
+    logger "SOF-ELK post-dhcp lease: issueupdate_config() - IP=${IP}"
     cat /etc/issue.stock | sed -e "s/<%IP%>/$IP/" > /etc/issue
     skill -9 agetty
 }
 
 issueupdate_restore() {
-    logger "SOF-ELK post-dhcp lease: issueupdate_restore)"
+    IP=$( ip address show $( ip route|grep default | awk '{print $5}' ) | awk '/inet / {print $2}' | cut -d '/' -f 1 )
+    logger "SOF-ELK post-dhcp lease: issueupdate_restore() - IP=${IP}"
     echo > /dev/null
 }
