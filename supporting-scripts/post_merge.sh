407d3941f06717dbe15004177a1f73270372b246
Phil Hagen <phil@lewestech.com>
2019-04-29 23:01:33 -0400

    bring filebeat config under management

--- supporting-scripts/post_merge.sh
+++ supporting-scripts/post_merge.sh
@@ -45,6 +45,11 @@ done
 /usr/bin/systemctl restart elastalert
 
 # restart filebeat to account for any new config files and/or prospectors
+FILEBEAT_CONF_PATH=/etc/filebeat/filebeat.yml
+if [ -a  $FILEBEAT_CONF_PATH ]; then
+    rm -f $FILEBEAT_CONF_PATH
+fi
+ln -fs /usr/local/sof-elk/lib/configfiles/filebeat.yml $FILEBEAT_CONF_PATH
 /usr/bin/systemctl restart filebeat
 
 # other housecleaning
