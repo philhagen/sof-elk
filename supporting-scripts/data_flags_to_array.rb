098d518c4cba977262d429c01f834b03639f31ba
Phil Hagen <phil@lewestech.com>
2019-05-18 14:21:57 -0400

    more field name lowercasing

--- supporting-scripts/data_flags_to_array.rb
+++ supporting-scripts/data_flags_to_array.rb
@@ -13,67 +13,67 @@ def filter(event)
 
     # set up the default fields
     headerdata_perms = {
-        "_RawValue" => headerdata_perms_orig,
-        "HasTargetIdList" => false,
-        "HasLinkInfo" => false,
-        "HasName" => false,
-        "HasRelativePath" => false,
-        "HasWorkingDir" => false,
-        "HasArguments" => false,
-        "HasIconLocation" => false,
-        "IsUnicode" => false,
-        "ForceNoLinkInfo" => false,
-        "HasExpString" => false,
-        "RunInSeparateProcess" => false,
-        "Reserved0" => false,
-        "HasDarwinId" => false,
-        "RunAsUser" => false,
-        "HasExpIcon" => false,
-        "NoPidlAlias" => false,
-        "Reserved1" => false,
-        "RunWithShimLayer" => false,
-        "ForceNoLinkTrack" => false,
-        "EnableTargetMetadata" => false,
-        "DisableLinkPathTracking" => false,
-        "DisableKnownFolderTracking" => false,
-        "DisableKnownFolderAlias" => false,
-        "AllowLinkToLink" => false,
-        "UnaliasOnSave" => false,
-        "PreferEnvironmentPath" => false,
-        "KeepLocalIdListForUncTarget" => false
+        "_rawvalue" => headerdata_perms_orig,
+        "hastargetidlist" => false,
+        "haslinkinfo" => false,
+        "hasname" => false,
+        "hasrelativepath" => false,
+        "hasworkingdir" => false,
+        "hasarguments" => false,
+        "hasiconlocation" => false,
+        "isunicode" => false,
+        "forcenolinkinfo" => false,
+        "hasexpstring" => false,
+        "runinseparateprocess" => false,
+        "reserved0" => false,
+        "hasdarwinid" => false,
+        "runasuser" => false,
+        "hasexpicon" => false,
+        "nopidlalias" => false,
+        "reserved1" => false,
+        "runwithshimlayer" => false,
+        "forcenolintrack" => false,
+        "enabletargetmetadata" => false,
+        "disablelinkpathtracking" => false,
+        "disableknownfoldertracking" => false,
+        "disableknownfolderalias" => false,
+        "allowlinktolink" => false,
+        "unaliasonsave" => false,
+        "preferenvironmentpath" => false,
+        "keeplocalidlistforunctarget" => false
     }
 
     # set true/false flags for each bit in the mask
-    headerdata_perms["HasTargetIdList"] = true if (headerdata_perms_orig & 0x0001 != 0)
-    headerdata_perms["HasLinkInfo"] = true if (headerdata_perms_orig & 0x0002 != 0)
-    headerdata_perms["HasName"] = true if (headerdata_perms_orig & 0x0004 != 0)
-    headerdata_perms["HasRelativePath"] = true if (headerdata_perms_orig & 0x008 != 0)
-    headerdata_perms["HasWorkingDir"] = true if (headerdata_perms_orig & 0x0010 != 0)
-    headerdata_perms["HasArguments"] = true if (headerdata_perms_orig & 0x0020 != 0)
-    headerdata_perms["HasIconLocation"] = true if (headerdata_perms_orig & 0x0040 != 0)
-    headerdata_perms["IsUnicode"] = true if (headerdata_perms_orig & 0x0080 != 0)
-    headerdata_perms["ForceNoLinkInfo"] = true if (headerdata_perms_orig & 0x0100 != 0)
-    headerdata_perms["HasExpString"] = true if (headerdata_perms_orig & 0x0200 != 0)
-    headerdata_perms["RunInSeparateProcess"] = true if (headerdata_perms_orig & 0x0400 != 0)
-    headerdata_perms["Reserved0"] = true if (headerdata_perms_orig & 0x0800 != 0)
-    headerdata_perms["HasDarwinId"] = true if (headerdata_perms_orig & 0x1000 != 0)
-    headerdata_perms["RunAsUser"] = true if (headerdata_perms_orig & 0x2000 != 0)
-    headerdata_perms["HasExpIcon"] = true if (headerdata_perms_orig & 0x4000 != 0)
-    headerdata_perms["NoPidlAlias"] = true if (headerdata_perms_orig & 0x8000 != 0)
-    headerdata_perms["Reserved1"] = true if (headerdata_perms_orig & 0x010000 != 0)
-    headerdata_perms["RunWithShimLayer"] = true if (headerdata_perms_orig & 0x020000 != 0)
-    headerdata_perms["ForceNoLinkTrack"] = true if (headerdata_perms_orig & 0x040000 != 0)
-    headerdata_perms["EnableTargetMetadata"] = true if (headerdata_perms_orig & 0x080000 != 0)
-    headerdata_perms["DisableLinkPathTracking"] = true if (headerdata_perms_orig & 0x100000 != 0)
-    headerdata_perms["DisableKnownFolderTracking"] = true if (headerdata_perms_orig & 0x200000 != 0)
-    headerdata_perms["DisableKnownFolderAlias"] = true if (headerdata_perms_orig & 0x400000 != 0)
-    headerdata_perms["AllowLinkToLink"] = true if (headerdata_perms_orig & 0x8000000 != 0)
-    headerdata_perms["UnaliasOnSave"] = true if (headerdata_perms_orig & 0x10000000 != 0)
-    headerdata_perms["PreferEnvironmentPath"] = true if (headerdata_perms_orig & 0x20000000 != 0)
-    headerdata_perms["KeepLocalIdListForUncTarget"] = true if (headerdata_perms_orig & 0x40000000 != 0)
+    headerdata_perms["hastargetidlist"] = true if (headerdata_perms_orig & 0x0001 != 0)
+    headerdata_perms["haslinkinfo"] = true if (headerdata_perms_orig & 0x0002 != 0)
+    headerdata_perms["hasname"] = true if (headerdata_perms_orig & 0x0004 != 0)
+    headerdata_perms["hasrelativepath"] = true if (headerdata_perms_orig & 0x008 != 0)
+    headerdata_perms["hasworkingdir"] = true if (headerdata_perms_orig & 0x0010 != 0)
+    headerdata_perms["hasarguments"] = true if (headerdata_perms_orig & 0x0020 != 0)
+    headerdata_perms["hasiconlocation"] = true if (headerdata_perms_orig & 0x0040 != 0)
+    headerdata_perms["isunicode"] = true if (headerdata_perms_orig & 0x0080 != 0)
+    headerdata_perms["forcenolinkinfo"] = true if (headerdata_perms_orig & 0x0100 != 0)
+    headerdata_perms["hasexpstring"] = true if (headerdata_perms_orig & 0x0200 != 0)
+    headerdata_perms["runinseparateprocess"] = true if (headerdata_perms_orig & 0x0400 != 0)
+    headerdata_perms["reserved0"] = true if (headerdata_perms_orig & 0x0800 != 0)
+    headerdata_perms["hasdarwinid"] = true if (headerdata_perms_orig & 0x1000 != 0)
+    headerdata_perms["runasuser"] = true if (headerdata_perms_orig & 0x2000 != 0)
+    headerdata_perms["hasexpicon"] = true if (headerdata_perms_orig & 0x4000 != 0)
+    headerdata_perms["nopidlalias"] = true if (headerdata_perms_orig & 0x8000 != 0)
+    headerdata_perms["reserved1"] = true if (headerdata_perms_orig & 0x010000 != 0)
+    headerdata_perms["runwithshimlayer"] = true if (headerdata_perms_orig & 0x020000 != 0)
+    headerdata_perms["forcenolinktrack"] = true if (headerdata_perms_orig & 0x040000 != 0)
+    headerdata_perms["enabletargetmetadata"] = true if (headerdata_perms_orig & 0x080000 != 0)
+    headerdata_perms["disablelinkpathtracking"] = true if (headerdata_perms_orig & 0x100000 != 0)
+    headerdata_perms["disableknownfoldertracking"] = true if (headerdata_perms_orig & 0x200000 != 0)
+    headerdata_perms["disableknownfolderalias"] = true if (headerdata_perms_orig & 0x400000 != 0)
+    headerdata_perms["allowlinktolink"] = true if (headerdata_perms_orig & 0x8000000 != 0)
+    headerdata_perms["unaliasonsave"] = true if (headerdata_perms_orig & 0x10000000 != 0)
+    headerdata_perms["preferenvironmentpath"] = true if (headerdata_perms_orig & 0x20000000 != 0)
+    headerdata_perms["keeplocalidlistforunctarget"] = true if (headerdata_perms_orig & 0x40000000 != 0)
 
     # set a tag if there are any extraneous flags not addressed above
-    event.tag("_HeaderDataFlagsParseFailure") if (headerdata_perms_orig & 0x70FFFFFF != headerdata_perms_orig)
+    event.tag("_headerdataflagsparsefailure") if (headerdata_perms_orig & 0x70FFFFFF != headerdata_perms_orig)
 
     event.set(@source_field, headerdata_perms)
 
--- supporting-scripts/ntfs_flags_to_array.rb
+++ supporting-scripts/ntfs_flags_to_array.rb
@@ -13,51 +13,51 @@ def filter(event)
 
     # set up the default fields
     file_perms = {
-        "_RawValue" => file_perms_orig,
-        "ReadOnly" => false,
-        "Hidden" => false,
-        "System" => false,
-        "Archive" => false,
-        "Device" => false,
-        "Normal" => false,
-        "Temporary" => false,
-        "SparseFile" => false,
-        "ReparsePoint" => false,
-        "Compressed" => false,
-        "Offline" => false,
-        "NotContentIndexed" => false,
-        "Encrypted" => false,
-        "IntegrityStream" => false,
-        "Virtual" => false,
-        "NoScrubData" => false,
-        "HasEa" => false,
-        "IsDirectory" => false,
-        "IsIndexView" => false
+        "_rawvalue" => file_perms_orig,
+        "readonly" => false,
+        "hidden" => false,
+        "system" => false,
+        "archive" => false,
+        "device" => false,
+        "normal" => false,
+        "remporary" => false,
+        "sparsefile" => false,
+        "reparsepoint" => false,
+        "compressed" => false,
+        "offline" => false,
+        "notcontentindexed" => false,
+        "encrypted" => false,
+        "integritystream" => false,
+        "virtual" => false,
+        "noscrubdata" => false,
+        "hasea" => false,
+        "isdirectory" => false,
+        "isindexview" => false
     }
 
     # set true/false flags for each bit in the mask
-    file_perms["ReadOnly"] = true if (file_perms_orig & 0x0001 != 0)
-    file_perms["Hidden"] = true if (file_perms_orig & 0x0002 != 0)
-    file_perms["System"] = true if (file_perms_orig & 0x0004 != 0)
-    file_perms["Archive"] = true if (file_perms_orig & 0x0020 != 0)
-    file_perms["Device"] = true if (file_perms_orig & 0x0040 != 0)
-    file_perms["Normal"] = true if (file_perms_orig & 0x0080 != 0)
-    file_perms["Temporary"] = true if (file_perms_orig & 0x0100 != 0)
-    file_perms["SparseFile"] = true if (file_perms_orig & 0x0200 != 0)
-    file_perms["ReparsePoint"] = true if (file_perms_orig & 0x0400 != 0)
-    file_perms["Compressed"] = true if (file_perms_orig & 0x0800 != 0)
-    file_perms["Offline"] = true if (file_perms_orig & 0x1000 != 0)
-    file_perms["NotContentIndexed"] = true if (file_perms_orig & 0x2000 != 0)
-    file_perms["Encrypted"] = true if (file_perms_orig & 0x4000 != 0)
-    file_perms["IntegrityStream"] = true if (file_perms_orig & 0x8000 != 0)
-    file_perms["Virtual"] = true if (file_perms_orig & 0x010000 != 0)
-    file_perms["NoScrubData"] = true if (file_perms_orig & 0x020000 != 0)
-    file_perms["HasEa"] = true if (file_perms_orig & 0x040000 != 0)
-    file_perms["IsDirectory"] = true if (file_perms_orig & 0x10000000 != 0)
-    file_perms["IsIndexView"] = true if (file_perms_orig & 0x20000000 != 0)
+    file_perms["readonly"] = true if (file_perms_orig & 0x0001 != 0)
+    file_perms["hidden"] = true if (file_perms_orig & 0x0002 != 0)
+    file_perms["system"] = true if (file_perms_orig & 0x0004 != 0)
+    file_perms["archive"] = true if (file_perms_orig & 0x0020 != 0)
+    file_perms["device"] = true if (file_perms_orig & 0x0040 != 0)
+    file_perms["normal"] = true if (file_perms_orig & 0x0080 != 0)
+    file_perms["temporary"] = true if (file_perms_orig & 0x0100 != 0)
+    file_perms["sparsefile"] = true if (file_perms_orig & 0x0200 != 0)
+    file_perms["reparsepoint"] = true if (file_perms_orig & 0x0400 != 0)
+    file_perms["compressed"] = true if (file_perms_orig & 0x0800 != 0)
+    file_perms["offline"] = true if (file_perms_orig & 0x1000 != 0)
+    file_perms["notcontentindexed"] = true if (file_perms_orig & 0x2000 != 0)
+    file_perms["encrypted"] = true if (file_perms_orig & 0x4000 != 0)
+    file_perms["integritystream"] = true if (file_perms_orig & 0x8000 != 0)
+    file_perms["virtual"] = true if (file_perms_orig & 0x010000 != 0)
+    file_perms["noscrubdata"] = true if (file_perms_orig & 0x020000 != 0)
+    file_perms["hasea"] = true if (file_perms_orig & 0x040000 != 0)
+    file_perms["isdirectory"] = true if (file_perms_orig & 0x10000000 != 0)
+    file_perms["isindexview"] = true if (file_perms_orig & 0x20000000 != 0)
 
     # set a tag if there are any extraneous flags not addressed above
-    event.tag("_SiFlagsParseFailure") if (file_perms_orig & 0x3007FFE7 != file_perms_orig)
+    event.tag("_siflagsparsefailure") if (file_perms_orig & 0x3007FFE7 != file_perms_orig)
 
     event.set(@source_field, file_perms)
 
