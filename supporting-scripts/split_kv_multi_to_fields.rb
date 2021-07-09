c5e37fabc331dd50257a58d2f2cbb8cc8f746106
Phil Hagen <phil@lewestech.com>
2021-07-09 15:22:09 -0400

    changes for azure structure.  close #227.  However, full extent of requested functionality in nested/arrayed objects is not feasible at this time.

---
+++ supporting-scripts/split_kv_multi_to_fields.rb
@@ -0,0 +1,42 @@
+# SOF-ELK® Supporting script
+# (C)2021 Lewes Technology Consulting, LLC
+#
+# This script takes a hash of "{ name: x, value1: y, value2: z }" elements and creates "x: { y, z }" fields
+# for example:
+# - source: [{"Name":"Identity","Value1":"jvandyne","Value2":"admin"}]
+# - result: [{"Identity":{"Value1":"jvandyne","Value2":"admin"}]
+
+# the value of `params` is the value of the hash passed to `script_params`
+# in the logstash configuration
+def register(params)
+    @source_field = params["source_field"]
+    @destination_field = params["destination_field"]
+    @key_field = params["key_field"]
+end
+
+# the filter method receives an event and must return a list of events.
+# Dropping an event means not including it in the return array,
+# while creating new ones only requires you to add a new instance of
+# LogStash::Event to the returned array
+def filter(event)
+    # if source field is not present
+    if event.get(@source_field).nil?
+        event.tag("#{@source_field}_not_found")
+        return [event]
+    end
+    source_array = event.get(@source_field)
+
+    # create empty hash to hold new result
+    output = Hash.new()
+
+    for item in source_array
+        new_value = Marshal.load(Marshal.dump(item))
+        key = new_value.delete(@key_field)
+
+        output[key] = new_value
+    end
+
+    event.set(@destination_field, output)
+
+    return [event]
+end
