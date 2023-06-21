b6c5edc54a9664db864c0e8b7ac84063cb5824c1
Phil Hagen <phil@lewestech.com>
2023-06-22 00:19:45 +0200

    Use old geoip database files (#280)
    * use geoip db files with old license at build time so they are not zero'ed
    
    * streamline downloading old-license geoip dbs via local copy
    
    * fix hashes - previously were hashes of the tarballs, not the mmdb files themselves
    
    * correct pre-distro logic to reset mmdb files to intended versions and not empty ones
    
    * improve wording
    
    * remove empty geoip database files

--- supporting-scripts/distro_prep.sh
+++ supporting-scripts/distro_prep.sh
@@ -83,10 +83,10 @@ rm -rf ~elk_user/.viminfo
 echo "cleaning temp directories"
 rm -rf ~elk_user/tmp/*
 
-echo "Resetting GeoIP databases to empty."
+echo "Resetting GeoIP databases to distributed versions."
 for GEOIPDB in ASN City Country; do
     rm -f /usr/local/share/GeoIP/GeoLite-${GEOIPDB}.mmdb
-    cp -a /usr/local/sof-elk/supporting-scripts/geoip_bootstrap/empty-GeoLite2-${GEOIPDB}.mmdb /usr/local/share/GeoIP/GeoLite2-${GEOIPDB}.mmdb
+    curl -s -L -o /usr/local/share/GeoIP/GeoLite-${GEOIPDB}.mmdb https://lewestech.com/dist/GeoLite2-${GEOIPDB}.mmdb
 done
 rm -f /etc/GeoIP.conf
 rm -f /etc/cron.d/geoipupdate
