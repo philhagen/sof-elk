47e27b2022c376d9ecbb3f0ee3814a5fc9c22fa3
Phil Hagen <phil@lewestech.com>
2024-01-24 16:18:14 -0500

    use "domain-stats" more consistently

--- ansible/roles/domain_stats/files/gunicorn_config.py
+++ ansible/roles/domain_stats/files/gunicorn_config.py
@@ -3,8 +3,8 @@ import gunicorn
 import multiprocessing
 import os
 
-os.environ["SERVER_SOFTWARE"] = "domain_stats"
+os.environ["SERVER_SOFTWARE"] = "domain-stats"
 bind = "127.0.0.1:5730"
 workers = 9
 threads = 12
-gunicorn.SERVER_SOFTWARE = 'domain_stats'
+gunicorn.SERVER_SOFTWARE = 'domain-stats'
