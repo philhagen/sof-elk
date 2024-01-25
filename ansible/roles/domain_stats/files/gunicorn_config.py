956a5b28c60dfcb0a2c050660951b6576195fdd8
Phil Hagen <phil@lewestech.com>
2024-01-24 20:15:47 -0500

    migrate visualizations to lenses where possible

--- ansible/roles/domain_stats/files/gunicorn_config.py
+++ ansible/roles/domain_stats/files/gunicorn_config.py
@@ -3,8 +3,8 @@ import gunicorn
 import multiprocessing
 import os
 
-os.environ["SERVER_SOFTWARE"] = "domain-stats"
+os.environ["SERVER_SOFTWARE"] = "domain_stats"
 bind = "127.0.0.1:5730"
 workers = 9
 threads = 12
-gunicorn.SERVER_SOFTWARE = 'domain-stats'
+gunicorn.SERVER_SOFTWARE = 'domain_stats'
