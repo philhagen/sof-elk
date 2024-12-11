c9d66cd94fb69b6b1cddcf01608c6eb89247c461
Phil Hagen <phil@lewestech.com>
2024-12-11 14:37:54 -0500

    update for ubuntu

--- supporting-scripts/distro_prep.sh
+++ supporting-scripts/distro_prep.sh
@@ -40,18 +40,23 @@ git branch
 echo "ACTION REQUIRED!  Is this the correct branch?  (Should be 'public/v*', 'class/v*', or e.g. 'for123/v*' with  all others removed.)"
 read
 
-curl -s -XGET 'http://localhost:9200/_cat/indices/'|sort
+curl -s -XGET 'http://localhost:9200/_cat/indices/' | grep -v " \.internal\| \.kibana" | sort
 echo "ACTION REQUIRED!  The data above is still stored in elasticsearch.  Press return if this is correct or Ctrl-C to quit."
 read
 
 echo "The following logs and subdirectories are still present in the ingest directory.  Press return if this is correct or Ctrl-C to quit."
-find /logstash/ -type f -print
 find /logstash/ -mindepth 2 -type d
 read
 
-echo "The following contents are in ~elk_user/.ssh/.  Presss return if this is correct or Ctrl-C to quit."
-find ~elk_user/.ssh/ -print
+echo "The following users are defined in /etc/password.  Press return if this is correct or Ctrl-C to quit."
+awk -F: '$3>=1000 && $3<65000 {print "- "$1}'
 read
+
+if [ -d ~elk_user/.ssh/ ]; then
+    echo "The following contents are in ~elk_user/.ssh/.  Press return if this is correct or Ctrl-C to quit."
+    find ~elk_user/.ssh/ -print
+    read
+fi
 
 echo "updating local git repo clones"
 cd /usr/local/sof-elk/
@@ -64,34 +69,25 @@ echo "cleaning apt caches"
 apt-get clean
 
 echo "cleaning user home directories"
-rm -f ~root/.bash_history
-rm -f ~elk_user/.bash_history
-rm -f ~root/.python_hisory
-rm -f ~elk_user/.python_history
-rm -f ~root/.lesshst
-rm -f ~elk_user/.lesshst
-rm -rf ~root/.local
-rm -rf ~elk_user/.local
-rm -rf ~root/.cache
-rm -rf ~elk_user/.cache
-rm -rf ~root/.vim
-rm -rf ~elk_user/.vim
-rm -rf ~root/.viminfo
-rm -rf ~elk_user/.viminfo
-rm -rf ~root/.bundle
-rm -rf ~elk_user/.bundle
-rm -rf ~root/.ansible
-rm -rf ~elk_user/.ansible
-rm -rf ~root/.config
-rm -rf ~elk_user/.config
-rm -rf ~root/.vscode-server
-rm -rf ~elk_user/.vscode-server
+for userclean in root elk_user; do
+    rm -f ~${userclean}/.bash_history
+    rm -f ~${userclean}/.python_history
+    rm -f ~${userclean}/.lesshst
+    rm -rf ~${userclean}/.local
+    rm -rf ~${userclean}/.cache
+    rm -rf ~${userclean}/.vim
+    rm -rf ~${userclean}/.viminfo
+    rm -rf ~${userclean}/.bundle
+    rm -rf ~${userclean}/.ansible
+    rm -rf ~${userclean}/.config
+    rm -rf ~${userclean}/.vscode-server
+done
 #cat /dev/null > ~/.bash_history; history -c ; history -w; exit
 
 echo "cleaning temp directories"
 rm -rf ~elk_user/tmp/*
 
-echo "Reseting GeoIP databases to distributed versions."
+echo "Resetting GeoIP databases to distributed versions."
 for GEOIPDB in ASN City Country; do
     rm -f /usr/local/share/GeoIP/GeoLite2-${GEOIPDB}.mmdb
     curl -s -L -o /usr/local/share/GeoIP/GeoLite2-${GEOIPDB}.mmdb https://lewestech.com/dist/GeoLite2-${GEOIPDB}.mmdb
@@ -120,7 +116,7 @@ rm -f /etc/cron.d/geoipupdate
 # curl -s -XDELETE 'http://127.0.0.1:9200/elastalert_status_status' > /dev/null
 # elastalert-create-index --host 127.0.0.1 --port 9200 --no-ssl --no-auth --url-prefix "" --index "elastalert_status" --old-index "" --config /etc/sysconfig/elastalert_config.yml
 
-echo "reload kibana dashboards"
+echo "reloading kibana dashboards"
 /usr/local/sbin/load_all_dashboards.sh
 
 echo "stopping kibana"
@@ -135,7 +131,7 @@ echo "removing elasticsearch .tasks index"
 curl -s -XDELETE 'http://localhost:9200/.tasks' > /dev/null
 
 echo "stopping network"
-systemctl stop network
+ifconfig ens33 down
 
 echo "stopping elasticsearch"
 systemctl stop elasticsearch
@@ -144,12 +140,12 @@ echo "stopping logstash"
 systemctl stop logstash
 
 echo "clearing SSH Host Keys"
-systemctl stop sshd
+systemctl stop ssh.socket
 rm -f /etc/ssh/*key*
 
 echo "clearing cron/at content"
 systemctl stop atd
-systemctl stop crond
+systemctl stop cron
 rm -f /var/spool/cron/atjobs/.SEQ
 rm -rf /var/spool/cron/atjobs/*
 
