f06901c376bb4732781d1664cab3611b9dfc23b7
Phil Hagen <phil@lewestech.com>
2024-12-16 19:26:20 -0500

    new class branch naming

--- supporting-scripts/distro_prep.sh
+++ supporting-scripts/distro_prep.sh
@@ -37,7 +37,7 @@ fi
 echo "Checking that we're on the correct SOF-ELK® branch"
 cd /usr/local/sof-elk/
 git branch
-echo "ACTION REQUIRED!  Is this the correct branch?  (Should be 'public/v*', 'class/v*', or e.g. 'for123/v*' with  all others removed.)"
+echo "ACTION REQUIRED!  Is this the correct branch?  (Should be 'public/v*' or 'class/for123/v*' with  all others removed.)"
 read
 
 indices=$( curl -s -XGET 'http://localhost:9200/_cat/indices/' | grep -v " \.internal\| \.kibana" | sort )
