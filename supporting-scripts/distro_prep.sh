19c6f8a1a7ad720a603fc72cfb87648fc8989e3a
Phil Hagen <phil@lewestech.com>
2024-12-18 14:57:27 -0500

    don't download distro-ready GeoIP databases unless needed

--- supporting-scripts/distro_prep.sh
+++ supporting-scripts/distro_prep.sh
@@ -101,10 +101,20 @@ echo "cleaning temp directories"
 rm -rf ~elk_user/tmp/*
 
 echo "Resetting GeoIP databases to distributed versions."
+declare -A md5values
+md5values["ASN"]="c20977100c0a6c0842583ba158e906ec"
+md5values["City"]="4c60b3acf2e6782d48ce2b42979f7b98"
+md5values["Country"]="849e7667913e375bb3873f8778e8fb17"
 for GEOIPDB in ASN City Country; do
-    rm -f /usr/local/share/GeoIP/GeoLite2-${GEOIPDB}.mmdb
-    curl -s -L -o /usr/local/share/GeoIP/GeoLite2-${GEOIPDB}.mmdb https://sof-elk.com/dist/GeoLite2-${GEOIPDB}.mmdb
-    chmod 644 /usr/local/share/GeoIP/GeoLite2-${GEOIPDB}.mmdb
+    file=GeoLite2-${GEOIPDB}.mmdb
+    local_path=/usr/local/share/GeoIP/${file}
+    md5=$( md5sum ${local_path} | awk '{print $1}' )
+    if [ ${md5} != ${md5values[${GEOIPDB}]} ]; then
+        echo "- ${local_path}"
+        rm -f ${local_path}
+        curl -s -L -o ${local_path} https://sof-elk.com/dist/${file}
+        chmod 644 ${local_path}
+    fi
 done
 rm -f /etc/GeoIP.conf
 rm -f /etc/cron.d/geoipupdate
