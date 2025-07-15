6563f819c13f9ec9a4db1305fca6129894a9f0c1
Phil Hagen <phil@lewestech.com>
2025-07-15 10:34:48 -0400

    fix the horrendously broken tcp flags logic

--- supporting-scripts/tcp_flags_to_array.rb
+++ supporting-scripts/tcp_flags_to_array.rb
@@ -1,5 +1,5 @@
 # SOF-ELK® Supporting script
-# (C)2024 Lewes Technology Consulting, LLC
+# (C)2025 Lewes Technology Consulting, LLC
 #
 # This script creates an array of TCP flags from an integer (or hex value)
 
@@ -25,40 +25,47 @@ def filter(event)
         # build a string
         tcp_flags_str = ""
         tcp_flags.each { |flag| tcp_flags_str += "#{flag[0].upcase}" }
+
+        # now that the string is created, treat the derived string as such for the rest of the process
+        @source_type = "str"
+
+    elsif @source_type == "str"
+        tcp_flags_str= event.get(@source_field)
     end
 
-    if @source_type == "str" || @source_type == "arr"
+    if @source_type == "str"
         # calculate the integer value
         tcp_flags_int = 0
-        tcp_flags_int += 1 if tcp_flags_source.include?("F")
-        tcp_flags_int += 2 if tcp_flags_source.include?("S")
-        tcp_flags_int += 4 if tcp_flags_source.include?("R")
-        tcp_flags_int += 8 if tcp_flags_source.include?("P")
-        tcp_flags_int += 16 if tcp_flags_source.include?("A")
-        tcp_flags_int += 32 if tcp_flags_source.include?("U") 
-        tcp_flags_int += 64 if tcp_flags_source.include?("E")
-        tcp_flags_int += 128 if tcp_flags_source.include?("C")
-
-        event.remove("[netflow][tcp_control_bits]")
-        event.set("netflow.tcp_control_bits", tcp_flags_int)
+        tcp_flags_int += 1 if tcp_flags_str.include?("F")
+        tcp_flags_int += 2 if tcp_flags_str.include?("S")
+        tcp_flags_int += 4 if tcp_flags_str.include?("R")
+        tcp_flags_int += 8 if tcp_flags_str.include?("P")
+        tcp_flags_int += 16 if tcp_flags_str.include?("A")
+        tcp_flags_int += 32 if tcp_flags_str.include?("U")
+        tcp_flags_int += 64 if tcp_flags_str.include?("E")
+        tcp_flags_int += 128 if tcp_flags_str.include?("C")
     end
 
-    if @source_type == "int" || @source_type == "str"
-        # Create array based on values in the bitmask
-        tcp_flags = Array.new()
-        tcp_flags.push("cwr") if (tcp_flags_int & 0x80 != 0)
-        tcp_flags.push("ece") if (tcp_flags_int & 0x40 != 0)
-        tcp_flags.push("urg") if (tcp_flags_int & 0x20 != 0)
-        tcp_flags.push("ack") if (tcp_flags_int & 0x10 != 0)
-        tcp_flags.push("psh") if (tcp_flags_int & 0x08 != 0)
-        tcp_flags.push("rst") if (tcp_flags_int & 0x04 != 0)
-        tcp_flags.push("syn") if (tcp_flags_int & 0x02 != 0)
-        tcp_flags.push("fin") if (tcp_flags_int & 0x01 != 0)
+    # Create array based on values in the bitmask
+    tcp_flags = Array.new()
+    tcp_flags.push("cwr") if (tcp_flags_int & 0x80 != 0)
+    tcp_flags.push("ece") if (tcp_flags_int & 0x40 != 0)
+    tcp_flags.push("urg") if (tcp_flags_int & 0x20 != 0)
+    tcp_flags.push("ack") if (tcp_flags_int & 0x10 != 0)
+    tcp_flags.push("psh") if (tcp_flags_int & 0x08 != 0)
+    tcp_flags.push("rst") if (tcp_flags_int & 0x04 != 0)
+    tcp_flags.push("syn") if (tcp_flags_int & 0x02 != 0)
+    tcp_flags.push("fin") if (tcp_flags_int & 0x01 != 0)
 
-        event.remove("[network][tcp_flags]")
-        event.set("network.tcp_flags", tcp_flags)
-    end
+    # set the field for the array
+    event.remove("[network][tcp_flags]")
+    event.set("network.tcp_flags", tcp_flags)
 
+    # set the field for the integer
+    event.remove("[netflow][tcp_control_bits]")
+    event.set("netflow.tcp_control_bits", tcp_flags_int)
+
+    # set the field for the hex version
     event.remove("[network.tcp_flags_hex]")
     event.set("network.tcp_flags_hex", "0x" + tcp_flags_int.to_s(16).upcase)
 
