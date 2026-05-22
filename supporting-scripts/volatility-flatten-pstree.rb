6787825360ac946b1568eb5b8053cfbc48cb2e13
Phil Hagen <phil@lewestech.com>
2026-05-22 15:49:07 -0400

    cleanup

--- supporting-scripts/volatility-flatten-pstree.rb
+++ supporting-scripts/volatility-flatten-pstree.rb
@@ -11,19 +11,25 @@ end
 # this is a recursive function that will add each individual process structure to the "result" array,
 #   then traverse down the __children array, if present+populated.  depth will be assigned at each level,
 #   starting with 0
-def parse_process_tree(process, depth = 0)
+def flatten_process_tree(process, depth = 0)
   result = []
 
   process['process_depth'] = depth
 
   if process['__children'].is_a?(Array) && process['__children'].length > 0
+
+    # peel off the __children field and add what's left at the top level to the result array
     children = process.delete('__children')
     result << process
+
+    # traverse the child processes, adding the output from each iteration to the result array
     children.each do |child_process|
-      result += parse_process_tree(child_process, depth+1)
+      result += flatten_process_tree(child_process, depth + 1)
     end
+
   else
     # this occurs when there is no __children or __children==[]
+    # if so, add what's at the top-level to the result array
     process.delete('__children')
     result << process
   end
@@ -32,7 +38,7 @@ def parse_process_tree(process, depth = 0)
 end
 
 def filter(event)
-  # Tag and quit if source field isn't present
+  # Tag and exit if source field isn't present
   if event.get("#{@source_field}").nil?
     event.tag("#{@source_field}_not_found")
     return [event]
@@ -41,9 +47,9 @@ def filter(event)
   # Retrieve the source
   source = event.get("#{@source_field}")
 
-  flatlist = parse_process_tree(source)
+  flat_process_list = flatten_process_tree(source)
 
-  event.set("#{@target_field}", flatlist)
+  event.set("#{@target_field}", flat_process_list)
   return [event]
 end
 
@@ -56,7 +62,8 @@ test "when no child processes" do
     events.first.get("[output][0][Audit]") == nil &&
     events.first.get("[output][0][Cmd]") == nil &&
     events.first.get("[output][0][PID]") == 4 &&
-    events.first.get("[output][0][process_depth]") == 0
+    events.first.get("[output][0][process_depth]") == 0 &&
+    events.first.get("[output][0][__children]") == nil
   end
 end
 
@@ -69,11 +76,13 @@ test "when one child process" do
     events.first.get("[output][0][Cmd]") == nil &&
     events.first.get("[output][0][PID]") == 4 &&
     events.first.get("[output][0][process_depth]") == 0 &&
+    events.first.get("[output][0][__children]") == nil &&
 
     events.first.get("[output][1][Audit]") == "MemCompression" &&
     events.first.get("[output][1][Cmd]") == "MemCompression" &&
     events.first.get("[output][1][PID]") == 1816 &&
-    events.first.get("[output][1][process_depth]") == 1
+    events.first.get("[output][1][process_depth]") == 1 &&
+    events.first.get("[output][1][__children]") == nil
   end
 end
 
@@ -86,16 +95,19 @@ test "when multiple child processes at same level" do
     events.first.get("[output][0][Cmd]") == nil &&
     events.first.get("[output][0][PID]") == 4 &&
     events.first.get("[output][0][process_depth]") == 0 &&
+    events.first.get("[output][0][__children]") == nil &&
 
     events.first.get("[output][1][Audit]") == "MemCompression" &&
     events.first.get("[output][1][Cmd]") == "MemCompression" &&
     events.first.get("[output][1][PID]") == 1816 &&
     events.first.get("[output][1][process_depth]") == 1 &&
+    events.first.get("[output][1][__children]") == nil &&
 
     events.first.get("[output][2][Audit]") == "Registry" &&
     events.first.get("[output][2][Cmd]") == "Registry" &&
     events.first.get("[output][2][PID]") == 92 &&
-    events.first.get("[output][2][process_depth]") == 1
+    events.first.get("[output][2][process_depth]") == 1 &&
+    events.first.get("[output][2][__children]") == nil
   end
 end
 
@@ -107,21 +119,25 @@ test "when multiple child processes at different levels" do
     events.first.get("[output][0][Cmd]") == nil &&
     events.first.get("[output][0][PID]") == 4 &&
     events.first.get("[output][0][process_depth]") == 0 &&
+    events.first.get("[output][0][__children]") == nil &&
 
     events.first.get("[output][1][Audit]") == "MemCompression" &&
     events.first.get("[output][1][Cmd]") == "MemCompression" &&
     events.first.get("[output][1][PID]") == 1816 &&
     events.first.get("[output][1][process_depth]") == 1 &&
+    events.first.get("[output][1][__children]") == nil &&
 
     events.first.get("[output][2][Audit]") == "Registry" &&
     events.first.get("[output][2][Cmd]") == "Registry" &&
     events.first.get("[output][2][PID]") == 92 &&
     events.first.get("[output][2][process_depth]") == 2 &&
+    events.first.get("[output][2][__children]") == nil &&
 
     events.first.get("[output][3][Audit]") == "\\Device\\HarddiskVolume3\\Windows\\System32\\RuntimeBroker.exe" &&
     events.first.get("[output][3][Cmd]") == "C:\\Windows\\System32\\RuntimeBroker.exe -Embedding" &&
     events.first.get("[output][3][PID]") == 7852 &&
-    events.first.get("[output][3][process_depth]") == 1
+    events.first.get("[output][3][process_depth]") == 1 &&
+    events.first.get("[output][3][__children]") == nil
   end
 end
 
